630b4ec8cd9c93fb0fd6a32ef1a0c2a14c64f14d
bettyego <nwabethroseonuoha@gmail.com>
2026-05-13 12:59:55 +0100

    made some update

--- src/component/page/AboutUs.jsx
+++ src/component/page/AboutUs.jsx
@@ -15,7 +15,7 @@ const videos = [
    { src: '/His.mp4', title: 'His Royal Majesty is One' },
      { src: '/georgette and fredrick .MP4', title: 'Georgette and Frederick' },
    { src: '/ZION1.MP4', title: 'Zion Birthday' },
-    { src: '/lead.mp4', title: 'The woman who bravely leads' },
+    // { src: '/lead.mp4', title: 'The woman who bravely leads' },
    
   
   
